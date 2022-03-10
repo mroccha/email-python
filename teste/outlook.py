4d686bacf274739325c8c5fa9c7829aefb01be0b
mroccha <marianarocha14@outlook.com>
2022-03-10 09:28:37 -0300

    teste email
    Primeiro pull request

---
+++ teste/outlook.py
@@ -0,0 +1,37 @@
+
+import pyautogui
+import time
+
+def bot():
+
+    pyautogui.press('winleft')
+    time.sleep(1)
+    pyautogui.write('Outlook')
+    time.sleep(2)
+    pyautogui.press('enter')
+    time.sleep(2)
+    pyautogui.hotkey('ctrl','o')
+    time.sleep(1)
+    pyautogui.write('marianarocha1424@gmail.com')
+    pyautogui.press('tab')
+    pyautogui.press('tab')
+    pyautogui.write("Inmetrics")
+    pyautogui.press('tab')
+    pyautogui.write('Olá Inmetrics!')
+    pyautogui.hotkey('ctrl', 'enter')
+    time.sleep(8)
+    pyautogui.hotkey('alt', 'space')
+    pyautogui.press('up')
+    time.sleep(2)
+    pyautogui.press('enter')
+    time.sleep(1)
+    pyautogui.press('enter')
+    time.sleep(2)
+    pyautogui.press('tab')
+    pyautogui.press('enter')
+
+
+bot()
+
+
+
